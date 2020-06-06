af82726fb77fa57fefdcac0076d145c4474e34be
cgallo <mail@cristiangallo.com.ar>
2020-06-06 03:51:10 -0300

    usuarios

--- src/dataDB/DBUser.java
+++ src/dataDB/DBUser.java
@@ -19,7 +19,7 @@ public class DBUser {
         ResultSet rs = null;
         try {
             stmt = ConexionDB.getInstancia().getConexion().prepareStatement(
-                    "select id, username, first_name, last_name, email, password, is_staff, is_active, " +
+                    "select id, username, password, first_name, last_name, email, is_staff, is_active, " +
                             "is_superuser, last_login, date_joined from user where username = ? and password = ?"
             );
             stmt.setString(1, username);
@@ -29,7 +29,7 @@ public class DBUser {
                 user = new User(rs.getInt("id"), rs.getString("username"), rs.getString("password"),
                         rs.getString("first_name"), rs.getString("last_name"), rs.getString("email"),
                         rs.getBoolean("is_staff"), rs.getBoolean("is_active"), rs.getBoolean("is_superuser"),
-                        rs.getDate("last_login"), rs.getDate("last_login"));
+                        rs.getDate("last_login"), rs.getDate("date_joined"));
 
             }
         } catch (SQLException e) {
@@ -44,5 +44,39 @@ public class DBUser {
             ConexionDB.getInstancia().releaseConexion();
         }
         return user;
+    }
+
+    public static void save(User user) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = ConexionDB.getInstancia().getConexion().prepareStatement(
+                    "insert into user(username, password, first_name, last_name, email, is_staff, is_active, " +
+                            "is_superuser, last_login, date_joined) values (?,?,?,?,?,?,?,?,?,?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS
+            );
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getFirstName());
+            stmt.setString(4, user.getLastName());
+            stmt.setString(5, user.getEmail());
+            stmt.setBoolean(6, user.getIsStaff());
+            stmt.setBoolean(7, user.getIsActive());
+            stmt.setBoolean(8, user.getIsSuperuser());
+            stmt.setDate(9, user.getLastLogin());
+            stmt.setDate(10, user.getDateJoined());
+            stmt.execute();
+            rs = stmt.getGeneratedKeys();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.cancel();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            ConexionDB.getInstancia().releaseConexion();
+        }
     }
 }
--- src/entidades/User.java
+++ src/entidades/User.java
@@ -36,6 +36,84 @@ public class User {
         this.date_joined = date_joined;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean getIsStaff() {
+        return is_staff;
+    }
+
+    public void setIsStaff(boolean is_staff) {
+        this.is_staff = is_staff;
+    }
+
+    public boolean getIsActive() {
+        return is_active;
+    }
+
+    public void setIsActive(boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public boolean getIsSuperuser() {
+        return is_superuser;
+    }
+
+    public void setIsSuperuser(boolean is_superuser) {
+        this.is_superuser = is_superuser;
+    }
+
+    public java.sql.Date getLastLogin() {
+        return (java.sql.Date) last_login;
+    }
+
+    public void setLastLogin(java.sql.Date last_login) {
+        this.last_login = last_login;
+    }
+
+    public java.sql.Date getDateJoined() {
+        return (java.sql.Date) date_joined;
+    }
+
+    public void setDateJoined(java.sql.Date date_joined) {
+        this.date_joined = date_joined;
+    }
 }
 
